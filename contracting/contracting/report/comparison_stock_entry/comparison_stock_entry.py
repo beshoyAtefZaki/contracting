5c48b24b94b69ee1e96431b9bae28721cacbb8e5
abanoubmounir77 <abanoubmounir07@gmail.com>
2023-10-22 17:26:19 +0300

    edit

--- contracting/contracting/report/comparison_stock_entry/comparison_stock_entry.py
+++ contracting/contracting/report/comparison_stock_entry/comparison_stock_entry.py
@@ -80,6 +80,12 @@ def get_columns(filters):
 			"width": 160,
 		},
 		{
+			"label": _("Total Qty"),
+			"fieldname": "total_qty",
+			"fieldtype": "Float",
+			"width": 160,
+		},
+		{
 			"label": _("FROM Warehouse"),
 			"fieldname": "from_warehouse",
 			"fieldtype": "Link",
@@ -99,6 +105,8 @@ def get_data(conditions, filters):
 	,`tabStock Entry Detail`.qty 
 	,`tabComparison`.customer
 	,`tabComparison Item`.clearance_item_name as main_item_name
+	,`tabComparison Item`.qty comparsion_qty
+	,(`tabStock Entry Detail`.qty * `tabComparison Item`.qty ) as total_qty
 	FROM `tabStock Entry`
 	INNER JOIN `tabStock Entry Detail`
 	ON `tabStock Entry Detail`.parent=`tabStock Entry`.name
@@ -112,10 +120,10 @@ def get_data(conditions, filters):
 	data = frappe.db.sql(sql,as_dict=1)
 	result = []
 	check_headers = []
-	key_func = lambda x: (x["main_item"], x["customer"],x['stock_entry_name']) 
+	key_func = lambda x: (x["main_item"], x["customer"],x['stock_entry_name'],x['comparsion_qty']) 
 	for key, group in itertools.groupby(data, key_func):
 		if key[2] not in check_headers: # stock entry not add before
-			head_row = {'main_item':key[0],'customer':key[1],'stock_entry_name':key[2],'header':True}
+			head_row = {'main_item':key[0],'customer':key[1],'stock_entry_name':key[2],'qty':key[3],'header':True}
 			result.append(head_row)
 			check_headers.append(key[2])
 		for child in list(group):
@@ -123,3 +131,6 @@ def get_data(conditions, filters):
 	result = sorted(result, key=lambda d: d['stock_entry_name'])  
 	# print (f'\n\n\nsorted=={result}\n\n')
 	return result or []
+
+
+
