349d6f5b2f19982a3bf2d02854df7e93d952101f
abanoubmounir <abanoubmounir07@gmail.com>
2023-11-23 09:22:21 +0200

    edit

--- contracting/contracting/report/comparison_stock_entry/comparison_stock_entry.py
+++ contracting/contracting/report/comparison_stock_entry/comparison_stock_entry.py
@@ -64,13 +64,21 @@ def get_columns(filters):
 			"options": "Item",
 			"width": 160,
 		},
-		# {
-		# 	"label": _("Main Item Name"),
-		# 	"fieldname": "main_item_name",
-		# 	"fieldtype": "Link",
-		# 	"options": "Item",
-		# 	"width": 160,
-		# },
+		{
+			"label": _("Child Item Name"),
+			"fieldname": "child_item_name",
+			"fieldtype": "Link",
+			"options": "Item",
+			"width": 160,
+		},
+		{
+			"label": _("comp_item_card"),
+			"fieldname": "comp_item_card",
+			"fieldtype": "Link",
+			"options": "Item",
+			"width": 160,
+		},
+		
 		{
 			"label": _("Child Item"),
 			"fieldname": "child_item",
@@ -92,8 +100,8 @@ def get_columns(filters):
 			"width": 160,
 		},
 		{
-			"label": _("Qty"),
-			"fieldname": "qty",
+			"label": _("Comparsion Stock Qty"),
+			"fieldname": "comparsion_stock_qty",
 			"fieldtype": "Float",
 			"width": 160,
 		},
@@ -150,49 +158,47 @@ def get_data(conditions,filters):
 	return data
 
 def get_all(conditions):
-	sql = """
-		select `tabStock Entry`.name as stock_entry_name_
-		,`tabStock Entry`.stock_entry_type 
-		,`tabStock Entry Detail`.item_code
-		,`tabComparison`.customer as cst
-		,CONCAT(`tabStock Entry Detail`.item_code ,":",`tabStock Entry Detail`.item_name)as child_item
-		,`tabStock Entry`.comparison_item as main_item
-		,`tabComparison Item Card Stock Item`.item as child_item_name
-		,`tabStock Entry`.from_warehouse
-		,`tabStock Entry`.to_warehouse
-		,(`tabStock Entry Detail`.qty ) as stock_qty
-		,1 as  main_item_qty 
-		,`tabComparison Item`.qty comparsion_qty
-		,`tabComparison Item Card Stock Item`.qty qty
-		,(`tabComparison Item Card Stock Item`.qty * `tabComparison Item`.qty)as total_qty
-		,((`tabStock Entry Detail`.qty / (`tabComparison Item Card Stock Item`.qty * `tabComparison Item`.qty) )*100) as comp_percent
-		,((`tabComparison Item Card Stock Item`.qty * `tabComparison Item`.qty) - `tabStock Entry Detail`.qty) as outstand_qty
-		,(((`tabComparison Item Card Stock Item`.qty * `tabComparison Item`.qty) - `tabStock Entry Detail`.qty)/100) as Outstand_percent
-		FROM `tabStock Entry`
-		INNER JOIN `tabStock Entry Detail`
-		ON `tabStock Entry Detail`.parent=`tabStock Entry`.name	
-		INNER JOIN `tabComparison Item Card`
-		ON `tabComparison Item Card`.comparison =`tabStock Entry`.comparison
-		AND `tabComparison Item Card`.item_code = `tabStock Entry`.comparison_item 
-		INNER JOIN `tabComparison Item Card Stock Item`
-			ON `tabComparison Item Card Stock Item`.parent=`tabComparison Item Card`.name
-			AND `tabComparison Item Card Stock Item`.item=`tabStock Entry Detail`.item_code
-		INNER JOIN `tabComparison`
-		ON `tabComparison`.name=`tabComparison Item Card`.comparison
-		INNER JOIN `tabComparison Item`
-		ON `tabComparison Item`.parent=`tabComparison Item Card`.comparison
-		where  `tabComparison Item Card`.docstatus=1 and `tabComparison`.docstatus=1
-		ORDER BY `tabStock Entry`.name
+	sql = f"""
+		select 
+			`tabStock Entry`.name as stock_entry_name_
+			,`tabStock Entry`.stock_entry_type 
+			,`tabStock Entry Detail`.item_code
+			,`tabComparison Item Card Stock Item`.item as child_item_name
+			,`tabComparison Item Card`.name as comp_item_card
+			,CONCAT(`tabStock Entry Detail`.item_code ,":",`tabStock Entry Detail`.item_name)as child_item
+			,`tabStock Entry`.comparison_item as main_item
+			,`tabStock Entry`.from_warehouse
+			,`tabStock Entry`.to_warehouse
+			,(`tabStock Entry Detail`.qty ) as stock_qty
+			,1 as  main_item_qty 
+			,`tabComparison Item Card`.item_comparison_number comparsion_qty
+			,`tabComparison Item Card Stock Item`.qty as comparsion_stock_qty
+			,(`tabComparison Item Card Stock Item`.qty * `tabComparison Item Card`.item_comparison_number)as total_qty
+			,((`tabStock Entry Detail`.qty / (`tabComparison Item Card Stock Item`.qty * `tabComparison Item Card`.item_comparison_number) )*100) as comp_percent
+			,((`tabComparison Item Card Stock Item`.qty * `tabComparison Item Card`.item_comparison_number) - `tabStock Entry Detail`.qty) as outstand_qty
+			,(((`tabComparison Item Card Stock Item`.qty * `tabComparison Item Card`.item_comparison_number) - `tabStock Entry Detail`.qty)/100) as Outstand_percent
+			FROM `tabStock Entry`
+				INNER JOIN `tabStock Entry Detail`
+					ON `tabStock Entry Detail`.parent=`tabStock Entry`.name	
+				INNER JOIN `tabComparison Item Card`
+					ON `tabComparison Item Card`.comparison =`tabStock Entry`.comparison
+					AND `tabComparison Item Card`.item_code = `tabStock Entry`.comparison_item 
+				INNER JOIN `tabComparison Item Card Stock Item`
+					ON `tabComparison Item Card Stock Item`.parent=`tabComparison Item Card`.name 
+					AND `tabComparison Item Card Stock Item`.item=`tabStock Entry Detail`.item_code 
+			where  `tabComparison Item Card`.docstatus=1   
+				AND `tabStock Entry`.docstatus=1 AND {conditions}
+			ORDER BY `tabStock Entry`.name
 	"""
 	data = frappe.db.sql(sql,as_dict=1)
 	result = []
 	check_headers = []
-	key_func = lambda x: (x["main_item"], x["cst"],x['stock_entry_name_'],x['main_item_qty'],x['comparsion_qty']) 
+	key_func = lambda x: (x["main_item"], x['stock_entry_name_'],) 
 	for key, group in itertools.groupby(data, key_func):
-		if key[2] not in check_headers: # stock entry not add before
-			head_row = {'main_item':key[0],'customer':key[1],'stock_entry_name':key[2],'qty':key[3],'total_qty':key[4],'header':True}
+		if key[1] not in check_headers: # stock entry not add before
+			head_row = {'main_item':key[0],'stock_entry_name':key[1],'header':True}
 			result.append(head_row)
-			check_headers.append(key[2])
+			check_headers.append(key[1])
 		for child in list(group):
 			result.append(child)
 	# result = sorted(result, key=lambda d: d['stock_entry_name'])  
@@ -244,7 +250,46 @@ def get_data_on_stock_entry_type(filters):
 			data = frappe.db.sql(sql,as_dict=1)
 			return data
 
-# f"""
+
+
+
+
+# dm2 = """
+# select `tabStock Entry`.name as stock_entry_name_
+# 		,`tabStock Entry`.stock_entry_type 
+# 		,`tabStock Entry Detail`.item_code
+# 		,`tabComparison`.customer as cst
+# 		,CONCAT(`tabStock Entry Detail`.item_code ,":",`tabStock Entry Detail`.item_name)as child_item
+# 		,`tabStock Entry`.comparison_item as main_item
+# 		,`tabComparison Item Card Stock Item`.item as child_item_name
+# 		,`tabStock Entry`.from_warehouse
+# 		,`tabStock Entry`.to_warehouse
+# 		,(`tabStock Entry Detail`.qty ) as stock_qty
+# 		,1 as  main_item_qty 
+# 		,`tabComparison Item`.qty comparsion_qty
+# 		,`tabComparison Item Card Stock Item`.qty qty
+# 		,(`tabComparison Item Card Stock Item`.qty * `tabComparison Item`.qty)as total_qty
+# 		,((`tabStock Entry Detail`.qty / (`tabComparison Item Card Stock Item`.qty * `tabComparison Item`.qty) )*100) as comp_percent
+# 		,((`tabComparison Item Card Stock Item`.qty * `tabComparison Item`.qty) - `tabStock Entry Detail`.qty) as outstand_qty
+# 		,(((`tabComparison Item Card Stock Item`.qty * `tabComparison Item`.qty) - `tabStock Entry Detail`.qty)/100) as Outstand_percent
+# 		FROM `tabStock Entry`
+# 		INNER JOIN `tabStock Entry Detail`
+# 		ON `tabStock Entry Detail`.parent=`tabStock Entry`.name	
+# 		INNER JOIN `tabComparison Item Card`
+# 		ON `tabComparison Item Card`.comparison =`tabStock Entry`.comparison
+# 		AND `tabComparison Item Card`.item_code = `tabStock Entry`.comparison_item 
+# 		INNER JOIN `tabComparison Item Card Stock Item`
+# 			ON `tabComparison Item Card Stock Item`.parent=`tabComparison Item Card`.name
+# 			AND `tabComparison Item Card Stock Item`.item=`tabStock Entry Detail`.item_code
+# 		INNER JOIN `tabComparison`
+# 		ON `tabComparison`.name=`tabComparison Item Card`.comparison
+# 		INNER JOIN `tabComparison Item`
+# 		ON `tabComparison Item`.parent=`tabComparison Item Card`.comparison
+# 		where  `tabComparison Item Card`.docstatus=1 and `tabComparison`.docstatus=1
+# 		AND `tabComparison Item Card Stock Item`.docstatus=1
+# 		AND`tabStock Entry`.name='MAT-STE-2023-00009'
+# 		ORDER BY `tabStock Entry`.name"""
+# dem1=f"""
 # 		select `tabStock Entry`.name as stock_entry_name_
 # 	,`tabStock Entry`.comparison
 # 	,`tabStock Entry`.comparison_item as main_item
